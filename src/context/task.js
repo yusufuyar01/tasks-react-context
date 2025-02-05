65f2c0f9c793febdc996f295b0111efc2ffa56f9
Yusuf UYAR <uyaryusuf01@gmail.com>
2025-02-05 15:43:58 +0300

    task işlemleri task.js'e geçti
    app.js içerisindeki görev ekleme ve silme gibi metodlar task.js içerisine component yapısına uygun şekilde geçirildi.

--- src/context/task.js
+++ src/context/task.js
@@ -1,9 +1,54 @@
 import { createContext } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const TaskContext = createContext():
 
 function Provider({children}){
-    <TaskContext.Provider>
+    const [tasks, setTasks] = useState([]);
+  const createTask = async (title,taskDesc) => {
+    const response = await axios.post('http://localhost:3001/tasks',{
+    title,
+    taskDesc
+    });
+    console.log(response)
+    const createdTasks = [...tasks,response.data];
+    setTasks(createdTasks);
+  };
+
+  const fetchTask = async () =>{
+    const response = await axios.get('http://localhost:3001/tasks');
+    setTasks(response.data);
+  };
+  const deleteTaskById = async (id) => {
+    await axios.delete(`http://localhost:3001/tasks/${id}`);
+    const afterDeletingTasks = tasks.filter((task) =>{
+      return task.id !== id;
+    })
+    setTasks(afterDeletingTasks); 
+  }
+  const editTaskById = async (id,updatedTitle,updatedTaskDesc) => {
+    await axios.put(`http://localhost:3001/tasks/${id}`,{
+      title:updatedTitle,
+      taskDesc:updatedTaskDesc
+    });
+    const afterUpdatingTasks = tasks.map((task) =>{
+      if (task.id===id) { 
+        return {id,title:updatedTitle,taskDesc:updatedTaskDesc};
+      }
+      return task;
+    })
+    setTasks(afterUpdatingTasks); 
+  }
+  const sharedValuesAndMethods{
+    tasks,
+    createTask,
+    fetchTask,
+    deleteTaskById,
+    editTaskById,
+  }
+
+    return <TaskContext.Provider value={sharedValuesAndMethods}>
         {children}
     </TaskContext.Provider>
 }
